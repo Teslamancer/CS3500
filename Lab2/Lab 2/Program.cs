e5876167c1cd6ec1903cd3e3a46c4a44d1762263
Hunter Schmidt <epicschmidt@gmail.com>
2019-08-28 15:32:43 -0600

    Initial Commit for Lab 2.

---
+++ Lab2/Lab 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    /// <summary>
+    /// Example Class for Lab 2
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Prints a short Lorem Ipsum
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lorem Ipsum Dolor")
+        }
+    }
+}
