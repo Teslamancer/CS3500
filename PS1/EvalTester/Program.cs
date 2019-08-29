af70913edb1564a45c8b0ffafb1476b3e5314314
Hunter Schmidt <epicschmidt@gmail.com>
2019-08-28 23:53:40 -0600

    Create, link and comment FormulaEvaluator and EvalTester Projects

---
+++ PS1/EvalTester/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FormulaEvaluator;
+
+namespace EvalTester
+{
+    /// <summary>
+    /// This class contains methods to test the functionality and implementation of the Evaluator class
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// This method runs all of the tests on the Evaluator class
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- PS1/FormulaEvaluator/Class1.cs
+++ PS1/FormulaEvaluator/Class1.cs
@@ -1,7 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace FormulaEvaluator
 {
+    /// <summary>
+    /// This class contains a static function to evaluate a string mathmatical formula given a functor 
+    /// to retrieve variable values
+    /// </summary>
     public static class Evaluator
     {
     }
