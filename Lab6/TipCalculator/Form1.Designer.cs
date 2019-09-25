41768ffda5851575d9e97daa94e9222e0273008a
Hunter Schmidt <u0916776@umail.utah.edu>
2019-09-25 15:31:45 -0600

    Made it pretty and Made it Auto-Calculate changes.

--- Lab6/TipCalculator/Form1.Designer.cs
+++ Lab6/TipCalculator/Form1.Designer.cs
@@ -31,74 +31,106 @@
             this.label1 = new System.Windows.Forms.Label();
             this.BillBox = new System.Windows.Forms.TextBox();
             this.TipBox = new System.Windows.Forms.TextBox();
-            this.CalculateButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.PercentBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TotalBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(145, 61);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(24, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.Size = new System.Drawing.Size(180, 46);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Enter Total Bill";
+            this.label1.Text = "Enter Bill";
             this.label1.Click += new System.EventHandler(this.Label1_Click);
             // 
             // BillBox
             // 
-            this.BillBox.Location = new System.Drawing.Point(350, 61);
+            this.BillBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.BillBox.Location = new System.Drawing.Point(335, 28);
             this.BillBox.Name = "BillBox";
-            this.BillBox.Size = new System.Drawing.Size(277, 26);
+            this.BillBox.Size = new System.Drawing.Size(292, 53);
             this.BillBox.TabIndex = 1;
+            this.BillBox.TextChanged += new System.EventHandler(this.BillBox_TextChanged);
             // 
             // TipBox
             // 
-            this.TipBox.Location = new System.Drawing.Point(350, 152);
+            this.TipBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.TipBox.Location = new System.Drawing.Point(335, 163);
             this.TipBox.Name = "TipBox";
-            this.TipBox.Size = new System.Drawing.Size(277, 26);
+            this.TipBox.ReadOnly = true;
+            this.TipBox.Size = new System.Drawing.Size(292, 53);
             this.TipBox.TabIndex = 2;
-            // 
-            // CalculateButton
-            // 
-            this.CalculateButton.Location = new System.Drawing.Point(138, 152);
-            this.CalculateButton.Name = "CalculateButton";
-            this.CalculateButton.Size = new System.Drawing.Size(118, 26);
-            this.CalculateButton.TabIndex = 3;
-            this.CalculateButton.Text = "Calculate Tip";
-            this.CalculateButton.UseVisualStyleBackColor = true;
-            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(149, 106);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.Location = new System.Drawing.Point(24, 100);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 20);
+            this.label2.Size = new System.Drawing.Size(290, 46);
             this.label2.TabIndex = 4;
             this.label2.Text = "Tip Percentage";
             // 
             // PercentBox
             // 
-            this.PercentBox.Location = new System.Drawing.Point(350, 106);
+            this.PercentBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.PercentBox.Location = new System.Drawing.Point(335, 93);
             this.PercentBox.Name = "PercentBox";
-            this.PercentBox.Size = new System.Drawing.Size(277, 26);
+            this.PercentBox.Size = new System.Drawing.Size(292, 53);
             this.PercentBox.TabIndex = 5;
+            this.PercentBox.TextChanged += new System.EventHandler(this.PercentBox_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label3.Location = new System.Drawing.Point(24, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 46);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Total";
+            // 
+            // TotalBox
+            // 
+            this.TotalBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.TotalBox.Location = new System.Drawing.Point(335, 228);
+            this.TotalBox.Name = "TotalBox";
+            this.TotalBox.ReadOnly = true;
+            this.TotalBox.Size = new System.Drawing.Size(292, 53);
+            this.TotalBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label4.Location = new System.Drawing.Point(24, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(224, 46);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tip Amount";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TotalBox);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.PercentBox);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.CalculateButton);
             this.Controls.Add(this.TipBox);
             this.Controls.Add(this.BillBox);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Tip Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,9 +141,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox BillBox;
         private System.Windows.Forms.TextBox TipBox;
-        private System.Windows.Forms.Button CalculateButton;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox PercentBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TotalBox;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Lab6/TipCalculator/Form1.cs
+++ Lab6/TipCalculator/Form1.cs
@@ -12,24 +12,76 @@ namespace TipCalculator
 {
     public partial class Form1 : Form
     {
+        private static double bill;
+        private static double tip;
+        private static double tipPercent;
+        private static double total;
+        private static bool validBillAmount;
+        private static bool validTipAmount;
+        private void calculate()
+        {
+            if(validBillAmount && validTipAmount)
+            {                
+                tip = bill * tipPercent/100;
+                total = bill + tip;
+                TipBox.Text = tip.ToString();
+                TotalBox.Text = total.ToString();
+            }
+            else
+            {
+                TipBox.Text = "";
+                TotalBox.Text = "";
+            }
+        }
         public Form1()
         {
             InitializeComponent();
+            bill = 0;
+            tip = 0;
+            validBillAmount = false;
+            validTipAmount = false;
         }
 
         private void Label1_Click(object sender, EventArgs e)
         {
 
+        }        
+
+        private void BillBox_TextChanged(object sender, EventArgs e)
+        {
+            double billamount;
+            if (Double.TryParse(BillBox.Text, out billamount))
+            {
+                bill = billamount;
+                validBillAmount = true;
+                calculate();
+            }
+            else
+            {
+                validBillAmount = false;
+                calculate();
+            }
         }
 
-        private void CalculateButton_Click(object sender, EventArgs e)
+        private void PercentBox_TextChanged(object sender, EventArgs e)
         {
-            double bill;
-            Double.TryParse(BillBox.Text, out bill);
-            double tip;
-            Double.TryParse(PercentBox.Text, out tip);
-            bill *= tip/100;
-            TipBox.Text = bill.ToString();
+            double tipamount;
+            if(Double.TryParse(PercentBox.Text, out tipamount))
+            {
+                tipPercent = tipamount;
+                validTipAmount = true;
+                calculate();
+            }
+            else
+            {
+                validTipAmount = false;
+                calculate();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
